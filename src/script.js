a231a64c92fa70fcc9cb70489eb3c3e0b14052b7
Shakanksh Sinha <shakankshs@gmail.com>
2023-04-13 13:52:23 +0530

    Production

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 /**
